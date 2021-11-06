72a7bfb17b5cd5a4c3c829d8651b64bba586861b
Ivan Nazarenko <IYNazarenko19@codingburgas.bg>
2021-11-06 10:01:44 +0200

    Add MoveHandle function

--- level/level/level/level.cpp
+++ level/level/level/level.cpp
@@ -24,3 +24,37 @@ char maze[10][10] = { {'#','=','#','#','#','#','#','#','#','#'},
 void ClearArea() {
     system("CLS");
 }
+void MoveHandle(int move, int& X, int& Y) {
+    switch (move = _getch()) {
+    case KEY_UP:
+        if (Y - 1 >= 0) {
+            if (maze[Y - 1][X] != '#') {
+                Y--;
+            }
+        }
+        break;
+
+    case KEY_DOWN:
+        if (Y + 1 <= 9) {
+            if (maze[Y + 1][X] != '#') {
+                Y++;
+            }
+        }
+        break;
+
+    case KEY_RIGHT:
+        if (X + 1 <= 9) {
+            if (maze[Y][X + 1] != '#') {
+                X++;
+            }
+        }
+        break;
+
+    case KEY_LEFT:
+        if (X - 1 >= 0) {
+            if (maze[Y][X - 1] != '#') {
+                X--;
+            }
+        }
+    }
+}
